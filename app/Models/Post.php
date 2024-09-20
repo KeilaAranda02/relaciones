7658a04cef1a3a494176565db9bfd8cb2b6a925a
Keila Aranda <keila.arandah@gmail.com>
2024-09-19 22:01:48 -0600

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,24 +10,33 @@ class Post extends Model
     use HasFactory;
     public function user()
     {
+        //un post pertenece a un usuario
         return $this->belongsTo(User::class);
     }
 
     public function category()
     {
+        //como pertenece a un usuario entonces pertenece a una categoria
+        //una categoria tiene muchos post
         return $this->belongsTo(Category::class);
     }
 
+    //METODOS POLIMORFICOS
     public function comments()
     {
+        //un post puede tener muchos comentarios. Relacion 1 a m
         return $this->morphMany(Comment::class, 'commentable');
     }
+
     public function images()
     {
+        
         return $this->morphOne(Image::class, 'imageable');
     }
+
     public function tags()
     {
+        //relacion m a m
         return $this->morphMany(Tag::class, 'taggable');
     }
 }
