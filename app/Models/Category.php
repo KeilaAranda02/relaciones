d8dc5f56b76c53a519200151aa3639f27eba645d
Keila Aranda <keila.arandah@gmail.com>
2024-09-19 22:01:50 -0600

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,12 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    //relacion entre categorias y post
     public function posts()
     {
+        //una categoria tiene muchos posts
         return $this->hasMany(Post::class);
     }
     public function videos()
     {
+        //tambien una categoria tiene muchos videos
         return $this->hasMany(Video::class);
     }
 }
