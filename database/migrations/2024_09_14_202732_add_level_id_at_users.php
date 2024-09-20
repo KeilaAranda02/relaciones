1891ff28195517ebca6f7bea84748a516fc153af
Keila Aranda <keila.arandah@gmail.com>
2024-09-19 20:38:32 -0600

    Update 2024_09_14_202732_add_level_id_at_users.php

--- database/migrations/2024_09_14_202732_add_level_id_at_users.php
+++ database/migrations/2024_09_14_202732_add_level_id_at_users.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table){
+            //se crea la nueva columna
             $table->bigInteger('level_id')->unsigned()
                     ->nullable()
                     ->after('id');
