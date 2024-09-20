42cd1633bcaf33903d36fc59df35cd8bc86fde48
Keila Aranda <keila.arandah@gmail.com>
2024-09-19 20:51:12 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+//Este código define una ruta que, al ser accedida, recupera todos los usuarios 
+//de la base de datos y los envía a la vista welcome, donde pueden ser mostrados.
+// Es una forma básica y común de manejar la lógica de solicitudes y respuestas en Laravel.
 Route::get('/', function () {
     $users = App\Models\User::get();
     return view('welcome', ['users' => $users]);
