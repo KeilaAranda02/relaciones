410dff9a74386a661ea1d79996e4fa8053acfa7b
Keila Aranda <keila.arandah@gmail.com>
2024-09-19 22:01:41 -0600

    Update 2024_09_14_220718_create_taggables_table.php

--- database/migrations/2024_09_14_220718_create_taggables_table.php
+++ database/migrations/2024_09_14_220718_create_taggables_table.php
@@ -14,14 +14,11 @@ return new class extends Migration
         Schema::create('taggables', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('tag_id')->unsigned();
-           
-
+        
              $table->morphs('taggable');
             
             $table->timestamps();
             
-
-
             $table->foreign('tag_id')->references('id')->on('tags')
             ->onDelete('cascade')
             ->onUpdate('cascade');
