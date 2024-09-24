e4edc9b682b9db4fd8bb01105a6a8ddb33a40b11
Keila Aranda <keila.arandah@gmail.com>
2024-09-23 19:21:54 -0600

    Update TagFactory.php

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -2,22 +2,31 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+//use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Tag;
+use Faker\Generator as Faker;
+
+$factory->define(Tag::class, function (Faker $faker){
+    return[
+        'name' => $faker->word
+    ];
+});
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
  */
-class TagFactory extends Factory
-{
+//class TagFactory extends Factory
+//{
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
+  //  public function definition(): array
+    //{
+      //  return [
             //
-        ];
-    }
-}
+        //];
+    //}
+//}
