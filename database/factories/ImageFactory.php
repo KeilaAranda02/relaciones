b3e218f40b425fe3d7e330f8a46c78561ad95518
Keila Aranda <keila.arandah@gmail.com>
2024-09-23 19:21:41 -0600

    Update ImageFactory.php

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -2,22 +2,32 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+//use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Image;
+use Faker\Generator as Faker;
+
+$factory->define(Image::class, function (Faker $faker){
+    return[
+        'url' => $faker->imageUrl(1024, 1024),
+    ];
+});
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
  */
-class ImageFactory extends Factory
-{
+//class ImageFactory extends Factory
+//{
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
+  //  public function definition(): array
+    //{
+      //  return [
             //
-        ];
-    }
-}
+        //];
+   // }
+//}
