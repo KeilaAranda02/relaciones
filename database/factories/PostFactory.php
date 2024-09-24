2d937d789dfa844fc90514441ee41ebc3dbc158e
Keila Aranda <keila.arandah@gmail.com>
2024-09-23 19:21:49 -0600

    Update PostFactory.php

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,22 +2,35 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+//use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker){
+    return[
+        'name' => $faker->sentence,
+        'category_id' => rand(1,4),
+        'user_id' => rand(1,5)
+    ];
+});
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
-class PostFactory extends Factory
-{
+//class PostFactory extends Factory
+//{
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
+  //  public function definition(): array
+    //{
+      //  return [
             //
-        ];
-    }
-}
+        //];
+    //}
+//}
