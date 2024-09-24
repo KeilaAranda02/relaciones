dc9c9cbefa18c6f396f14391eac5b8ff8f6c2771
Keila Aranda <keila.arandah@gmail.com>
2024-09-23 19:21:51 -0600

    Update ProfileFactory.php

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -2,22 +2,35 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+//use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+use App\Profile;
+use Faker\Generator as Faker;
+
+$factory->define(Profile::class, function (Faker $faker){
+    return[
+        'instagram' => $faker->userName,
+        'github' => $faker->userName,
+        'web' => $faker->web,
+    ];
+});
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Profile>
  */
-class ProfileFactory extends Factory
-{
+//class ProfileFactory extends Factory
+//{
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
+  //  public function definition(): array
+   // {
+     //   return [
             //
-        ];
-    }
-}
+       // ];
+    //}
+//}
