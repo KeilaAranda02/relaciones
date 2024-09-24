0c495a95a229b4e3314b591d28b187f278b6bd9d
Keila Aranda <keila.arandah@gmail.com>
2024-09-23 19:21:36 -0600

    Update CommentFactory.php

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,22 +2,35 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+//use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+use App\Comment;
+use Faker\Generator as Faker;
+
+$factory->define(Comment::class, function (Faker $faker){
+    return[
+        'body' => $faker->text,
+        'user_id' => rand(1,5)
+    ];
+});
+
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
  */
-class CommentFactory extends Factory
-{
+//class CommentFactory extends Factory
+//{
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
+  //  public function definition(): array
+    //{
+      //  return [
             //
-        ];
-    }
-}
+        //];
+    //}
+//}
