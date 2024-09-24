19fa15a6288539b794c36dffd711959c1f2f5d4e
Keila Aranda <keila.arandah@gmail.com>
2024-09-23 19:21:39 -0600

    Update GroupFactory.php

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -1,23 +1,33 @@
 <?php
-
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Group;
+use Faker\Generator as Faker;
+
+$factory->define(Group::class, function (Faker $faker){
+    return[
+        'name' => $faker->word
+    ];
+});
+
+
+
+//use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Group>
  */
-class GroupFactory extends Factory
-{
+//class GroupFactory extends Factory
+//{
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
+   // public function definition(): array
+    //{
+      //  return [
             //
-        ];
-    }
-}
+        //];
+    //}
+//}
