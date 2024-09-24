cfb261659fc20733f1465b16ee94ba0332a5d152
Keila Aranda <keila.arandah@gmail.com>
2024-09-23 19:21:59 -0600

    Update VideoFactory.php

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -2,22 +2,35 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+//use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+use App\Video;
+use Faker\Generator as Faker;
+
+$factory->define(Video::class, function (Faker $faker){
+    return[
+        'name' => $faker->sentence,
+        'category_id' => rand(1,4),
+        'user_id' => rand(1,5)
+    ];
+});
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Video>
  */
-class VideoFactory extends Factory
-{
+//class VideoFactory extends Factory
+//{
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
+  //  public function definition(): array
+    //{
+      //  return [
             //
-        ];
-    }
-}
+        //];
+    //}
+//}
