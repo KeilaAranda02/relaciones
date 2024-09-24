a20fe33b597c5d090e8975fbcb63dfdbe0391380
Keila Aranda <keila.arandah@gmail.com>
2024-09-23 19:21:43 -0600

    Update LevelFactory.php

--- database/factories/LevelFactory.php
+++ database/factories/LevelFactory.php
@@ -2,22 +2,31 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+//use Illuminate\Database\Eloquent\Factories\Factory;
 
+
+use App\Level;
+use Faker\Generator as Faker;
+
+$factory->define(Level::class, function (Faker $faker){
+    return[
+        //
+    ];
+});
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Level>
  */
-class LevelFactory extends Factory
-{
+//class LevelFactory extends Factory
+//{
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
+  //  public function definition(): array
+    //{
+      //  return [
             //
-        ];
-    }
-}
+        //];
+    //}
+//}
